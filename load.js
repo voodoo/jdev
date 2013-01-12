ed05016045a0b87a1dd5aa540633fa6c650312b7
Paul Vudmaska <paul.vudmaska@rackspace.com>
2013-01-11 19:17:54 -0600

    minor bug fix

--- load.js
+++ load.js
@@ -1,9 +1,14 @@
 (function(window){
   console.log('load')
   function main(){
-    var s = '<iframe id="iframe" frameborder="0" height="660px" width="350px" scrolling="no" style="padding:0px;position:absolute;top:10px;right:10px;z-index:999999999" border="0"></iframe>';
-    document.getElementsByTagName('body')[0].appendChild(s)
-    document.getElementById("iframe").setAttribute('src', "https://raw.github.com/voodoo/jdev/master/iframe.html");
+    var ifrm = document.createElement('IFRAME');
+        ifrm.src='https://raw.github.com/voodoo/jdev/master/iframe.html'
+        ifrm.width="350px"
+        ifrm.height="350px"
+    document.getElementsByTagName('body')[0].appendChild(ifrm)    
+    //var s = '<iframe id="iframe" frameborder="0" height="660px" width="350px" scrolling="no" style="padding:0px;position:absolute;top:10px;right:10px;z-index:999999999" border="0"></iframe>';
+    //document.getElementsByTagName('body')[0].appendChild(s)
+    //document.getElementById("iframe").setAttribute('src', "https://raw.github.com/voodoo/jdev/master/iframe.html");
   }
   main();
 })(window);
