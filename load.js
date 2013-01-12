490ab032d6fbf7d06b35be065fe0a7662b25d3aa
Paul Vudmaska <paul.vudmaska@gmail.com>
2013-01-11 20:01:37 -0600

    ready

--- load.js
+++ load.js
@@ -2,22 +2,8 @@
   console.log('load')
   function loader(src){
     var script = document.createElement('script');
-        script.src=src
-        document.getElementsByTagName('body')[0].appendChild(script)
-
+        script.src=src;
+        document.getElementsByTagName('body')[0].appendChild(script);
   }
-
   loader(src);
-
-  // function main(){
-  //   var ifrm = document.createElement('IFRAME');
-  //       ifrm.src='https://raw.github.com/voodoo/jdev/master/iframe.html'
-  //       ifrm.width="350px"
-  //       ifrm.height="350px"
-  //   document.getElementsByTagName('body')[0].appendChild(ifrm)
-  //   //var s = '<iframe id="iframe" frameborder="0" height="660px" width="350px" scrolling="no" style="padding:0px;position:absolute;top:10px;right:10px;z-index:999999999" border="0"></iframe>';
-  //   //document.getElementsByTagName('body')[0].appendChild(s)
-  //   //document.getElementById("iframe").setAttribute('src', "https://raw.github.com/voodoo/jdev/master/iframe.html");
-  // }
-  //main();
 })(window, prompt("Javascript Url(s)"));
